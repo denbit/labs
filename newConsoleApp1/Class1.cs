cae6a18a7fa9ccfbd8e69f7c4c157c603a42ca3f
denbit <den_b_it@i.ua>
2018-11-07 16:22:26 +0200

    create

---
+++ newConsoleApp1/Class1.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace newConsoleApp1
+{
+public class Class1
+{
+	public Class1()
+	{
+	}
+      public  void write() => Console.WriteLine("it is arrow function");
+        public void write(string s) => Console.WriteLine($"it is arrow function with param {s}");
+      public void cortage()
+        {
+            var cort = (3,name:4);
+            
+            write(Convert.ToString( cort.Item1));
+            write(Convert.ToString(cort.name));
+        }
+}
+}
+
---
+++ newConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace newConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 testing = new Class1();
+            testing.write();
+            Console.WriteLine("Enter param:");
+            string s = Console.ReadLine();
+            testing.write(s);
+            testing.cortage();
+            Console.ReadKey();
+        }
+    }
+}
